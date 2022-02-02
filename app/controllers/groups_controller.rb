1a6769d79475164820000abde6b17a844785e4eb
yooyama130 <yooyama.97.01@gmail.com>
2022-02-02 03:38:29 +0000

    [Finish] 応用課題7c（edit/show画面作成）

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
   def show
     @user = current_user
     @group = Group.find(params[:id])
+    @new_book = Book.new
   end
 
   def new
@@ -19,16 +20,23 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.owner_id = user.id
     if @group.save
-      redirect_to group_path(@group)
+      redirect_to groups_path
     else
       render 'new'
     end
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_path(@group)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
